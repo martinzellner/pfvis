26dfb4ab79f7869f75671790a97911dfcd47b1b6
Martin Zellner <martin.zellner@martinzellner.eu>
2016-06-15 17:48:11 +0200

    Reformatted Code

--- pfvis/__init__.py
+++ pfvis/__init__.py
@@ -1,1 +1,2 @@
-from .plots import plot_energy_price, plot_vargen_injection, plot_load_power, plot_battery_power, plot_battery_soc, plot_power
+from .plots import plot_energy_price, plot_vargen_injection, plot_load_power, plot_battery_power, plot_battery_soc, \
+    plot_power, plot_graph
